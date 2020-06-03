9e8f28c347e95ee26968b5a27271cfbddf41398f
Ronaldo Lopes <rlsanlo@gmail.com>
2020-06-03 18:11:26 -0300

    feat(create-point, search-result): Terminar página de cadastro de pontos de coleta e começar a página de resultados de locais pesquisados.

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,41 +1,88 @@
 // https://servicodados.ibge.gov.br/api/v1/localidades/estados
 
-function populateUfs(){
-    const ufSelect = document.querySelector("select[name=uf]");
+function populateUfs() {
+  const ufSelect = document.querySelector("select[name=uf]");
 
-    fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
-    .then( res => res.json() )
-    .then( states => {
-        for(const state of states) {
-            ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`
-        }
-    })
+  fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
+    .then((res) => res.json())
+    .then((states) => {
+      for (const state of states) {
+        ufSelect.innerHTML += `<option value="${state.id}">${state.nome}</option>`;
+      }
+    });
 }
 
 populateUfs();
 
-function getCities(event){
-    const citySelect = document.querySelector("[name=city]");
-    const stateInput = document.querySelector("[name=state]");
+function getCities(event) {
+  const citySelect = document.querySelector("[name=city]");
+  const stateInput = document.querySelector("[name=state]");
 
-    const ufValue = event.target.value;
+  const ufValue = event.target.value;
 
-    const indexOfSelectedState = event.target.selectedIndex;
-    stateInput.value = event.target.options[indexOfSelectedState].text
+  const indexOfSelectedState = event.target.selectedIndex;
+  stateInput.value = event.target.options[indexOfSelectedState].text;
 
-    const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
+  const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`;
 
-    fetch(url)
-    .then( res => res.json() )
-    .then( cities => {
-        for( const city of cities) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`;
-        }
+  citySelect.innerHTML = "<option value>Selecione a cidade</option> ";
+  citySelect.disabled = true;
 
-        citySelect.disabled = false;
-    })
+  fetch(url)
+    .then((res) => res.json())
+    .then((cities) => {
+      for (const city of cities) {
+        citySelect.innerHTML += `<option value="${city.nome}">${city.nome}</option>`;
+      }
+
+      citySelect.disabled = false;
+    });
 }
 
-document
-    .querySelector("select[name=uf]")
-    .addEventListener("change", getCities)
+document.querySelector("select[name=uf]").addEventListener("change", getCities);
+
+// Itens de coleta
+// pegar todos os li's
+const itemsToCollect = document.querySelectorAll(".items-grid li");
+
+for (let item of itemsToCollect) {
+  item.addEventListener("click", handleSelectedItem);
+}
+
+const colectedItems = document.querySelector("input[name=items");
+
+let selectedItems = [];
+
+function handleSelectedItem(event) {
+  const itemLi = event.target;
+
+  // adicionar ou remover uma classe com javaScript
+  itemLi.classList.toggle("selected");
+
+  const itemId = itemLi.dataset.id;
+
+  // Verificar se existem itens selecionados, se sim
+  // pegar os itens selecionados
+  const alreadySelected = selectedItems.findIndex((item) => {
+    const itemFound = item == itemId;
+    return itemFound;
+  });
+
+  // Se ja estiver selecionado
+  if (alreadySelected >= 0) {
+    // tirar da selecao
+    const filteredItems = selectedItems.filter((item) => {
+      const itemIsDifferent = item != itemId;
+      return itemIsDifferent;
+    });
+
+    selectedItems = filteredItems;
+  } else {
+    // se não estiver selecionado, adicionar à selecao
+    // adicionar a selecao
+    selectedItems.push(itemId);
+  }
+
+  // atualizar campos escondidos com os itens selecionados
+  colectedItems.value = selectedItems;
+}
